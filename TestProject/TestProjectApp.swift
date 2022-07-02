febc315e57c25831d2976e02192ce50dfd43d5f2
DipeshP-at-584098746410 <dipeshpokhrel.tech@gmail.com>
2022-07-02 11:04:03 +0530

    first Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Dipesh Pokhrel on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
